10fbd5d0c1f5f571f62f01df75aa173189b6641e
aparna-shegade <147500237+aparna-shegade@users.noreply.github.com>
2026-09-11 13:19:47 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int min=prices[0];
+        int maxProfit=0;
+        for(int i=1;i<n;i++)
+        {
+           int curr=prices[i]-min;
+           maxProfit=Math.max(maxProfit,curr);
+           min=Math.min(min,prices[i]);
+        }
+        return maxProfit;
+    }
+}
